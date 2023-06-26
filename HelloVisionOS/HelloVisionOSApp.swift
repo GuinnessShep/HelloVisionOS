0b380a274b3fdfb644c772993e03351d06ff5bb6
BomberFish <87151697+BomberFish@users.noreply.github.com>
2023-06-26 10:19:48 -0400

    Initial Commit

---
+++ HelloVisionOS/HelloVisionOSApp.swift
@@ -0,0 +1,17 @@
+//
+//  HelloVisionOSApp.swift
+//  HelloVisionOS
+//
+//  Created by Hariz Shirazi on 2023-06-26.
+//
+
+import SwiftUI
+
+@main
+struct HelloVisionOSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
